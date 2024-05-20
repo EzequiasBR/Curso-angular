8ed5ab5faf370b0a24efcde7ea94cd366f579d6d
EzequiasBR <ezequiashunk@gmail.com>
2024-05-20 17:36:39 -0300

    mas uma aula do curso angular da prof: Loiane Groner
    aulas 24,25,26

--- diretivas_04/src/app/app.module.ts
+++ diretivas_04/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivasNgswitchComponent } from './diretivas-ngswitch/diretivas-ngswitch.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivasNgswitchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
